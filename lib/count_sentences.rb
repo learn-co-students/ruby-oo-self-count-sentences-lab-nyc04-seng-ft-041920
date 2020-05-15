9c38284f4b910b3fba969b90f66d5ba12d9bacfb
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-15 02:20:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
+    
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    
+    self.end_with?("?")
 
   end
 
   def exclamation?
+        self.end_with?("!")
+
 
   end
-
   def count_sentences
+    new_array = self.split(/[.!?]/)
+    new_array.delete_if{|ele| ele.empty?}
+    return new_array.count
+    
+    binding.pry
 
   end
+
 end
